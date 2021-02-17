6028e5d204065e7a2bff0192eed25f6292075c52
Mateusz Grzybowski <mateusz.f.grzybowski@gmail.com>
2021-02-17 22:08:12 +0000

    Update - ML lesson

--- adaline.py
+++ adaline.py
@@ -1,11 +1,13 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from perceptron import plot_decision_regions
 
 df = pd.read_csv('/Users/matt/Desktop/ML/Perceptron/iris.data', header=None)
 y = df.iloc[0:100, 4].values  # iloc.values prints only values without description of columns. Here we are choosing the type of flower.
 y = np.where(y == 'Iris-setosa', -1, 1)  # returns elements chosen from x or y depending on condition. Here, if y is setosa.
 X = df.iloc[0:100, [0, 2]].values
+
 
 class AdalineGD(object):
     def __init__(self, eta=0.01, n_iter=50, random_state=1):
@@ -49,6 +51,26 @@ ada2 = AdalineGD(n_iter=10, eta=0.0001).fit(X, y)
 ax[1].plot(range(1, len(ada2.cost_) + 1),
            np.log10(ada2.cost_), marker='x')
 ax[1].set_xlabel('Iterations')
-ax[1].set_ylabel('Log (sum of square of errors)')
+ax[1].set_ylabel('Log (sum of squares of errors)')
 ax[1].set_title('Adaline - Learning coefficient 0.0001')
+plt.show()
+
+
+X_std = np.copy(X)
+X_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
+X_std[:, 1] = (X[:, 1] - X[:, 1].mean())/ X[:, 1].std()
+
+ada = AdalineGD(n_iter=15, eta=0.01)
+ada.fit(X_std, y)
+plot_decision_regions(X_std, y, classifier=ada)
+plt.title("Adaline - Gradient descent")
+plt.xlabel("Length of sepal [standardised]")
+plt.ylabel("Length of petal [standardised]")
+plt.legend(loc="upper left")
+plt.tight_layout()
+plt.show()
+
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker="o")
+plt.xlabel("Iterations")
+plt.ylabel("Sum of squares of errors")
 plt.show()
